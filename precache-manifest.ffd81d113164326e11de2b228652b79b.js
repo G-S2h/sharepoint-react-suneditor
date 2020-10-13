7900cdccea43cb18fc2cf4632672751a23cc8fa9
Salah <s-2-h@live.fr>
2020-10-13 10:00:49 +0200

    Updates

---
+++ precache-manifest.ffd81d113164326e11de2b228652b79b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcbbacf8afe29f3bd369c9a1d4c63b94",
+    "url": "./index.html"
+  },
+  {
+    "revision": "869e90c4909f2915dac7",
+    "url": "./static/css/2.b05ac691.chunk.css"
+  },
+  {
+    "revision": "54f22b636d05c6cd608a",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "869e90c4909f2915dac7",
+    "url": "./static/js/2.4b554ceb.chunk.js"
+  },
+  {
+    "revision": "246ad901f08703825af5eeab9ed7a0eb",
+    "url": "./static/js/2.4b554ceb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54f22b636d05c6cd608a",
+    "url": "./static/js/main.a28e5e75.chunk.js"
+  },
+  {
+    "revision": "5b5c355552d9d67ce13e",
+    "url": "./static/js/runtime-main.bd7aa7ab.js"
+  }
+]);
